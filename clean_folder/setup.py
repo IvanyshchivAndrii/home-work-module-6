d2c493337485a6768eaf41a1886e15750c7686fa
IvanyshchivAndrii <belka04011993@gmail.com>
2023-05-23 15:40:51 +0300

    add sutup to clean_folder

---
+++ clean_folder/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='clean_folder',
+    version='0.0.1',
+    description='This package can clen you folder',
+    url='http://github.com/dummy_user/useful',
+    author='Ivanyshchiv Andrii',
+    author_email='belka04011993@gmail.com',
+    license='MIT',
+    packages=find_namespace_packages(),
+    install_requires=['markdown'],
+)
